a0c55e3bc9eabb95a8df507c842d81645f11b375
abdelhamed alomar <abdelhamed.alomar.1996@gmail.com>
2019-11-26 11:46:34 +0100

    .

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturda
 btn.addEventListener("click",weekend)
 
 function weekend(){
-    para.innerHTML = "Hallo " + input.value + "."+ "Today is " + days[day] + " only " + (5 - day) + " Day to the Weekend"
+    para.innerHTML = "Hallo " + input.value + "."+ "Today is " + days[day]+ "!." + " only " + (5 - day) + " Day to the Weekend"
     btn.style.display = "none"
     input.style.display = "none"
 }
