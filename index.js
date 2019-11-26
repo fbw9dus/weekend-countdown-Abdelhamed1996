5d79ffd2503eb9a2e9b4a89d314b51d0239240dd
abdelhamed alomar <abdelhamed.alomar.1996@gmail.com>
2019-11-26 11:36:53 +0100

    Abdelhamed

---
+++ index.js
@@ -0,0 +1,16 @@
+var para = document.querySelector(".para")
+var input = document.querySelector(".input")
+var btn = document.querySelector(".btn")
+var day = new Date().getDay()
+
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+
+
+btn.addEventListener("click",weekend)
+
+function weekend(){
+    para.innerHTML = "Hallo " + input.value + "."+ "Today is " + days[day] + " only " + (5 - day) + " Day to the Weekend"
+    btn.style.display = "none"
+    input.style.display = "none"
+}
